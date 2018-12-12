e7e614992ad1a387f2a1fcbeaf76f99e07a2054b
ryangle <hcg1228@foxmail.com>
2018-12-12 10:49:42 +0800

    add project

---
+++ src/Lighthouse/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.IO;
+using System.Threading;
+using Akka.Actor;
+using Akka.Configuration;
+
+namespace Lighthouse
+{
+    class Program
+    {
+        static ManualResetEventSlim _manualResetEventSlim = new ManualResetEventSlim(false);
+
+        static void Main(string[] args)
+        {
+            Console.Title = "Lighthouse";
+            #region Akka config
+
+            Config actorConfig = null;
+            if (File.Exists("akka.conf"))
+            {
+                actorConfig = ConfigurationFactory.ParseString(File.ReadAllText("akka.conf"));
+            }
+            else
+            {
+                throw new ConfigurationException("not found akka.conf");
+            }
+
+            #endregion Akka config
+
+            var actorsystem = ActorSystem.Create("akka-lab", actorConfig);
+
+            //var deadletterWatchMonitorProps = Props.Create(() => new ActorSystemMonitor());
+            //var deadletterWatchActorRef = actorsystem.ActorOf(deadletterWatchMonitorProps);
+            //actorsystem.EventStream.Subscribe(deadletterWatchActorRef, typeof(DeadLetter));
+
+            _manualResetEventSlim.Wait();
+        }
+    }
+}
---
+++ src/ServiceNode1/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Threading;
+using Akka.Actor;
+using Akka.Cluster;
+
+namespace SeedNode1
+{
+    class Program
+    {
+        private static readonly log4net.ILog log = log4net.LogManager.GetLogger("akka-cluster", "seednode1");
+        private static readonly ManualResetEvent quitEvent = new ManualResetEvent(false);
+        private static readonly ManualResetEvent asTerminatedEvent = new ManualResetEvent(false);
+
+        /// <summary>
+        /// SEED NODE PROGRAM
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+            Console.CancelKeyPress += (sender, e) =>
+            {
+                quitEvent.Set();
+                e.Cancel = true;
+            };
+
+            log.Debug("Creating actor system");
+            var config = Akka.Configuration.ConfigurationFactory.ParseString(@"
+                akka {
+                    actor {
+                        provider = ""Akka.Cluster.ClusterActorRefProvider, Akka.Cluster""
+                        serializers {
+                            wire = ""Akka.Serialization.WireSerializer, Akka.Serialization.Wire""
+                        }
+                        serialization - bindings {
+                            ""System.Object"" = wire
+                        }
+                    }
+                    remote {
+                        helios.tcp {
+                            transport -class = ""Akka.Remote.Transport.Helios.HeliosTcpTransport, Akka.Remote""
+						    applied-adapters = []
+                            transport-protocol = tcp
+                            hostname = ""127.0.0.1""
+                            port = 0
+					    }
+                    }
+                    loggers = [""Akka.Logger.log4net.Log4NetLogger,Akka.Logger.log4net""]
+                    cluster {
+					    seed-nodes = [""akka.tcp://sample@127.0.0.1:7001""]
+                        roles = [seednode1]
+				    }
+			    }
+            ");
+            ActorSystem actorSystem = ActorSystem.Create("sample", config);
+
+            quitEvent.WaitOne();
+
+            log.Info("Shutting down");
+
+            var cluster = Cluster.Get(actorSystem);
+            cluster.RegisterOnMemberRemoved(() => MemberRemoved(actorSystem));
+            cluster.Leave(cluster.SelfAddress);
+
+            asTerminatedEvent.WaitOne();
+            log.Info("Actor system terminated, exiting");
+            if (System.Diagnostics.Debugger.IsAttached) Console.ReadLine();
+        }
+
+        private static async void MemberRemoved(ActorSystem actorSystem)
+        {
+            await actorSystem.Terminate();
+            asTerminatedEvent.Set();
+        }
+    }
+}
