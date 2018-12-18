175823fa73fa62e96403ff65c1248d9bd38051f5
ryangle <hcg1228@foxmail.com>
2018-12-18 16:15:32 +0800

    add shardingnode

---
+++ src/ShardingNode/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using Akka.Actor;
+using Akka.Cluster.Sharding;
+
+namespace ShardingNode
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            var system = ActorSystem.Create("akka-lab");
+            var region = ClusterSharding.Get(system).StartAsync(
+                typeName: "sharding-actor",
+                entityProps: Props.Create<MyActor>(),
+                settings: ClusterShardingSettings.Create(system),
+                messageExtractor: new MessageExtractor()).Result;
+
+            // send message to entity through shard region
+            region.Tell(new ShardEnvelope(shardId: 1, entityId: 1, message: "hello"));
+        }
+    }
+    public class MyActor : ReceiveActor
+    {
+
+    }
+    public sealed class ShardEnvelope
+    {
+        public readonly int ShardId;
+        public readonly int EntityId;
+        public readonly object Message;
+        public ShardEnvelope(int shardId, int entityId, object message)
+        {
+
+            ShardId = shardId;
+            EntityId = entityId;
+            Message = message;
+        }
+    }
+
+    // define, how shard id, entity id and message itself should be resolved
+    public sealed class MessageExtractor : IMessageExtractor
+    {
+        public string EntityId(object message) => (message as ShardEnvelope)?.EntityId.ToString();
+
+        public string ShardId(object message) => (message as ShardEnvelope)?.ShardId.ToString();
+
+        public object EntityMessage(object message) => (message as ShardEnvelope)?.Message;
+    }
+}
--- src/Terminal/Program.cs
+++ src/Terminal/Program.cs
@@ -20,19 +20,28 @@ namespace Terminal
                 port = args[0];
             }
             var actorSystem = ActorSystemFactory.Create(port);
-
-
-            var terminal = actorSystem.ActorOf<TerminalActor>("terminal");
-            var input = string.Empty;
-            while (input != "quit")
+            var cluster = Cluster.Get(actorSystem);
+            cluster.RegisterOnMemberUp(() =>
             {
-                input = Console.ReadLine();
-                terminal.Tell(new BinaryFunctionActor.Arguments { LeftArg = 10, RightArg = 20, Function = "add" });
-                Console.WriteLine($"send");
-            }
+                var terminal = actorSystem.ActorOf<TerminalActor>("terminal");
+                var input = string.Empty;
+                while (input != "quit")
+                {
+                    input = Console.ReadLine();
+                    terminal.Tell(new BinaryFunctionActor.Arguments
+                    {
+                        LeftArg = 10,
+                        RightArg = 20,
+                        Function = "add"
+                    });
+                    Console.WriteLine($"send");
+                }
+
+            });
+
             _manualResetEventSlim.Wait();
 
-            var cluster = Cluster.Get(actorSystem);
+            // var cluster = Cluster.Get(actorSystem);
             cluster.RegisterOnMemberRemoved(() => MemberRemoved(actorSystem));
             cluster.Leave(cluster.SelfAddress);
         }
