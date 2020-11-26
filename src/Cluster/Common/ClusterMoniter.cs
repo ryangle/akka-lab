03ecbcfb3dc131fb438e54e1dd36bf5f04c6800c
ryangle <hcg1228@foxmail.com>
2020-11-26 18:19:18 +0800

    update to net5.0

--- src/Cluster/Common/ClusterMoniter.cs
+++ src/Cluster/Common/ClusterMoniter.cs
@@ -2,6 +2,7 @@
 using Akka.Actor;
 using Akka.Cluster;
 using Akka.Remote;
+using Akka.Util.Internal;
 
 namespace Common
 {
@@ -51,31 +52,31 @@ namespace Common
             #region MemberStatusChange
             Receive<ClusterEvent.MemberExited>(msg =>
             {
-                ConsoleLog($"MemberExited,Member Address: {msg.Member.Address},{msg.Member.UniqueAddress}", ConsoleColor.DarkRed);
+                ConsoleLog($"MemberExited,Member Address: {msg.Member.Address},{msg.Member.UniqueAddress},Roles:{msg.Member.Roles.Join(",")}", ConsoleColor.DarkRed);
             });
             Receive<ClusterEvent.MemberJoined>(msg =>
             {
-                ConsoleLog($"MemberJoined,Member Address: {msg.Member.Address},{msg.Member.UniqueAddress}", ConsoleColor.DarkRed);
+                ConsoleLog($"MemberJoined,Member Address: {msg.Member.Address},{msg.Member.UniqueAddress},Roles:{msg.Member.Roles.Join(",")}", ConsoleColor.DarkRed);
             });
             Receive<ClusterEvent.MemberLeft>(msg =>
             {
-                ConsoleLog($"MemberLeft,Member Address:{msg.Member.Address},{msg.Member.UniqueAddress}", ConsoleColor.DarkRed);
+                ConsoleLog($"MemberLeft,Member Address:{msg.Member.Address},{msg.Member.UniqueAddress},Roles:{msg.Member.Roles.Join(", ")}", ConsoleColor.DarkRed);
             });
             Receive<ClusterEvent.MemberRemoved>(msg =>
             {
-                ConsoleLog($"MemberRemoved,Member Address: {msg.Member.Address},{msg.Member.UniqueAddress}", ConsoleColor.DarkRed);
+                ConsoleLog($"MemberRemoved,Member Address: {msg.Member.Address},{msg.Member.UniqueAddress},Roles:{msg.Member.Roles.Join(",")}", ConsoleColor.DarkRed);
             });
             Receive<ClusterEvent.MemberUp>(msg =>
             {
-                ConsoleLog($"MemberUp,Member Address:{msg.Member.Address},{msg.Member.UniqueAddress}", ConsoleColor.DarkRed);
+                ConsoleLog($"MemberUp,Member Address:{msg.Member.Address},{msg.Member.UniqueAddress},Roles:{msg.Member.Roles.Join(",")}", ConsoleColor.DarkRed);
             });
             Receive<ClusterEvent.MemberWeaklyUp>(msg =>
             {
-                ConsoleLog($"MemberWeaklyUp,Member Address: {msg.Member.Address},{msg.Member.UniqueAddress}", ConsoleColor.DarkRed);
+                ConsoleLog($"MemberWeaklyUp,Member Address: {msg.Member.Address},{msg.Member.UniqueAddress},Roles:{msg.Member.Roles.Join(",")}", ConsoleColor.DarkRed);
             });
             Receive<ClusterEvent.MemberStatusChange>(msg =>
             {
-                ConsoleLog($"MemberStatusChange,Member Address: {msg.Member.Address},{msg.Member.UniqueAddress}", ConsoleColor.DarkRed);
+                ConsoleLog($"MemberStatusChange,Member Address: {msg.Member.Address},{msg.Member.UniqueAddress},Roles:{msg.Member.Roles.Join(",")}", ConsoleColor.DarkRed);
             });
             #endregion
 
